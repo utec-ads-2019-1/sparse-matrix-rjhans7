1bee309c8ead0d7919ca730b7eb874da0a96d311
rjhans7 <32879845+rjhans7@users.noreply.github.com>
2019-06-18 16:16:31 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@
 #include "tester/tester.cpp"
 
 int main() {
-    //Tester::execute();
-
-        //indices inician desde 1 (FILA, COLUMNA)
+    Tester::execute();
+    /*
         auto myMatrix = new Matrix<int>(3, 3);
         auto myMatri2 = new Matrix<int>(3, 1);
         myMatrix->set(0, 0, 3);
@@ -21,18 +20,12 @@ int main() {
         myMatri2->set(0, 0, 1);
         myMatri2->set(1, 0, 2);
 
-        /*
-        int *myPointer = new int;
-        *myPointer = 7;
-        delete myPointer; //freed memory
-        myPointer = nullptr; //pointed dangling ptr to NULL
-*/
-
         myMatri2->print();
         //auto resultado = (*myMatrix) * (*myMatri2);
         //resultado.print();
         delete myMatrix;
         delete myMatri2;
+    */
 
     return EXIT_SUCCESS;
 }
